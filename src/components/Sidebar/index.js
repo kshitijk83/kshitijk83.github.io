40df8b0dc13ac87f1fe72c93f2fce60ccbf6afc4
kshitiz kumar <kshitiz1.kumar@paytm.com>
2022-08-20 23:46:22 +0530

    final draft 2

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,5 @@
 import './index.scss';
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faBriefcase, faCode, faEnvelope, faGear, faHome, faLightbulb, faUser } from '@fortawesome/free-solid-svg-icons';
